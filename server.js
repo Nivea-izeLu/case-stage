3b8b689badf702dd1da2ffbd01a2c6d3766fcc97
Nivea-izeLu <niveapontes02@gmail.com>
2025-08-09 22:09:53 -0300

    index finalizado

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: '1234',
-    database: 'IMPRESA_FICTICIA',
+    database: 'EMPRESA_FICTICIA',
     port: 3302
 });
 
